6dcdc3ab98fa0a1df028206ff789282e31f488cd
rombor <rombor.py@gmail.com>
2020-01-24 14:15:44 +0200

    Task 2.4

--- hw2/index.js
+++ hw2/index.js
@@ -4,7 +4,7 @@ let b = 2;
 console.log('a: ', a);
 console.log('b: ', b);
 
-console.log('a - b', a - b);
-console.log('a + b', a + b);
-console.log('a / b', a / b);
-console.log('a * b', a * b);
+console.log('a - b = ', a - b);
+console.log('a + b = ', a + b);
+console.log('a / b = ', a / b);
+console.log('a * b = ', a * b);
