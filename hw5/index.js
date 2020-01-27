67a1fb5f733c10c274889360b1d9ac74f6ece0f3
rombor <rombor.py@gmail.com>
2020-01-27 21:59:26 +0200

    Task 5.10

--- hw5/index.js
+++ hw5/index.js
@@ -1,14 +1,7 @@
-const sum = (from, to) => {
-    let result = 0;
-    for (let i = from; i <= to; i++) {
-        result += i;
+const findDivCount = (a, b, n) => {
+    for (let i = a; i <= b; i++) {
+        if (i % n === 0) {
+            console.log(i)
+        }
     }
-    return result
-}
-
-const compareSums = (a, b, c, d) => {
-    const firstSum = sum(a, b);
-    const secondSum = sum(c, d);
-
-    return firstSum > secondSum;
 }
