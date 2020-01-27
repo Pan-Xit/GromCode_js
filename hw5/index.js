f6936f6f3aa2f78e92f0257e438c3695f77d860d
rombor <rombor.py@gmail.com>
2020-01-27 21:33:24 +0200

    Task 5.8

--- hw5/index.js
+++ hw5/index.js
@@ -1,17 +1,1 @@
-const getPrimes = (number) => {
-    for (let i = 1; i <= number; i++) {
-        if (i <= 3) {
-            console.log(i)
-        } else if (i % 2 !== 0) {
-            let n = 3;
-            while (n <= i) {
-                if (n === i) {
-                    console.log(i)
-                } else if (i % n === 0) {
-                    break
-                } 
-                n += 2
-            }
-        }
-    }
-}
+const increaser = (a, index) => a > index ? a + index : a
