667812935cbcb3d1143e1cb7a7b01fdbd012b679
rombor <rombor.py@gmail.com>
2020-01-27 21:54:02 +0200

    Task 5.9

--- hw5/index.js
+++ hw5/index.js
@@ -1,1 +1,14 @@
-const increaser = (a, index) => a > index ? a + index : a
+const sum = (from, to) => {
+    let result = 0;
+    for (let i = from; i <= to; i++) {
+        result += i;
+    }
+    return result
+}
+
+const compareSums = (a, b, c, d) => {
+    const firstSum = sum(a, b);
+    const secondSum = sum(c, d);
+
+    return firstSum > secondSum;
+}
