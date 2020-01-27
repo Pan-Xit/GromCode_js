f20d1791815391251c0b29f78c87d63163ed1c15
rombor <rombor.py@gmail.com>
2020-01-27 12:29:30 +0200

    Task 4.7

--- hw3/index.js
+++ hw3/index.js
@@ -1,7 +1,10 @@
 let result = 0;
 
-for (let i = 0; i <= 10000000; i++) {
+for (let i = 1; i <= 1000; i++) {
     result += i
 }
 
-console.log(result)
+const reminder = result % 1234;
+const integer = (result - reminder) / 1234;
+
+console.log(integer > reminder)
