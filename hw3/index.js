0571fe538da63087eee37a2a7fbec7feff89dae0
rombor <rombor.py@gmail.com>
2020-01-27 12:18:51 +0200

    Task 4.6

--- hw3/index.js
+++ hw3/index.js
@@ -1,16 +1,7 @@
-/* Этот код менять не нужно */
-const defaultUserName = 'anonymus';
-const userName = 'John';
-const anotherUser = null;
-const isAdmin = false;
-const message = 'text';
-const anotherMessage = 'text as well';
-const isLoggedIn = Boolean(userName);
+let result = 0;
 
-/* В коде ниже нужно использовать правильные операторы после знака присваивания */
-const activeUser = anotherUser || defaultUserName;
-const hasAccess = isAdmin || isLoggedIn;
-const isTruthy = message < anotherMessage;
-const isFalsy = !isLoggedIn;
-const isNotTrue = typeof message === 'number';
-const isTrue = typeof message !== 'boolean';
+for (let i = 0; i <= 10000000; i++) {
+    result += i
+}
+
+console.log(result)
