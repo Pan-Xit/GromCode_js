fb3bcbc9da9209835643c91e0346597b678a22ee
rombor <rombor.py@gmail.com>
2020-01-27 12:35:12 +0200

    Task 4.5

--- hw3/index.js
+++ hw3/index.js
@@ -1,10 +1,14 @@
 let result = 0;
 
-for (let i = 1; i <= 1000; i++) {
-    result += i
+for (let i = 0; i <= 1000; i++) {
+    if (i % 2 != 0) {
+        console.log('Found');
+        result += i;
+    }
 }
 
-const reminder = result % 1234;
-const integer = (result - reminder) / 1234;
-
-console.log(integer > reminder)
+if (result * 5 > 5000) {
+    console.log('Bigger')
+} else {
+    console.log('Smaller or equal')
+}
