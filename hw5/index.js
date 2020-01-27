cdfdacd2a4e3270658a377edda085dcc4c5ad899
rombor <rombor.py@gmail.com>
2020-01-27 21:10:53 +0200

    Task 5.6

--- hw5/index.js
+++ hw5/index.js
@@ -1,9 +1,17 @@
-const getSum = (firstNumber, lastNumber) => {
-    let result = 0;
-    for (let number = firstNumber; number <= lastNumber; number++) {
-        if (number % 2 === 0) {
-            result += number;
+const getPrimes = (number) => {
+    for (let i = 1; i <= number; i++) {
+        if (i <= 3) {
+            console.log(i)
+        } else if (i % 2 !== 0) {
+            let n = 3;
+            while (n <= i) {
+                if (n === i) {
+                    console.log(i)
+                } else if (i % n === 0) {
+                    break
+                } 
+                n += 2
+            }
         }
-    };
-    return result;
+    }
 }
