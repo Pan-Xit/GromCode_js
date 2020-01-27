e6ddd8f8d78a7bffc6b7508b3d0427c3be38f1dd
rombor <rombor.py@gmail.com>
2020-01-27 12:04:10 +0200

    Task 4.5

--- hw4/index.js
+++ hw4/index.js
@@ -5,11 +5,15 @@ let result = 0;
 for (let i = m; i <= n; i++) {
     if (i % 5 == 0) {
         console.log(i)
-    } else if (i % 4 == 0) {
-        result *= i
-    } else if (i % 2 == 0) {
-        result += i
-    } else if (i % 3 == 0) {
+        continue
+    }
+    if (i % 2 == 0 && i % 4 != 0) {
+        result += i;
+    }
+    if (i % 3 == 0) {
         result -= i
+    }
+    if (i % 4 == 0) {
+        result *= i
     }
 }
