171904a9ec631b8fc12060626c2896cbc88a2d7b
rombor <rombor.py@gmail.com>
2020-01-27 18:18:28 +0200

    Task 4.5

--- hw4/index.js
+++ hw4/index.js
@@ -1,15 +1,21 @@
-const m = 11;
+const m = 0;
 const n = 20;
 let result = 0;
 
 for (let i = m; i <= n; i++) {
+    console.log(i)
     if (i % 5 == 0) {
         console.log(i)
-    } else if (i % 2 == 0 && i % 4 != 0) {
+    }
+    if (i % 2 == 0 && i % 4 != 0) {
+        // console.log('sum')
         result += i;
     } else if (i % 3 == 0) {
+        // console.log('sub')
         result -= i
     } else if (i % 4 == 0) {
+        // console.log('mul')
         result *= i;
     }
+    // console.log(`Result: `, result)
 }
