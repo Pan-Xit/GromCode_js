c2872851daa1559bcace10c1657bdeedca79f07a
rombor <rombor.py@gmail.com>
2020-01-27 20:02:32 +0200

    Task 5.3

--- hw5/index.js
+++ hw5/index.js
@@ -1,1 +1,9 @@
-const square = (num) => num * num
+const getSum = (firstNumber, lastNumber) => {
+    let result = 0;
+    for (let number = firstNumber; number <= lastNumber; number++) {
+        if (number % 2 === 0) {
+            result += number;
+        }
+    };
+    return result;
+}
