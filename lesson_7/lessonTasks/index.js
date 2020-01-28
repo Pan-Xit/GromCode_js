48ed99ac989159a91b75eedda10739e457789e70
rombor <rombor.py@gmail.com>
2020-01-28 22:07:28 +0200

    GromCode JS

--- lesson_7/lessonTasks/index.js
+++ lesson_7/lessonTasks/index.js
@@ -1,3 +1,3 @@
 const getMessagesForBestStudents = (allStudents, failedStudents) => 
     allStudents.filter(name => !failedStudents.includes(name))
-    .map(name => 'Good job ' + name);
+    .map(name => 'Good job, ' + name);
