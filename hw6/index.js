30462aeb12f31eb8f31538390cc030f5fb2696df
rombor <rombor.py@gmail.com>
2020-01-28 12:57:51 +0200

    GromCode JS

--- hw6/index.js
+++ hw6/index.js
@@ -1,10 +1,11 @@
 const squareArray = (arr) => {
     if (!Array.isArray(arr)) {
         return null
-    }
+    };
+
     let newArr = [];
     for (element of arr) {
         newArr.push(element * element)
-    }
-    return newArr
+    };
+    return newArr;
 }
