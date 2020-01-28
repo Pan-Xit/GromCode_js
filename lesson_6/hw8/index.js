e1aec413708faee37125e94f4c9e896524f4aca1
rombor <rombor.py@gmail.com>
2020-01-28 18:15:45 +0200

    GromCode JS

--- lesson_6/hw8/index.js
+++ lesson_6/hw8/index.js
@@ -1,8 +1,8 @@
 const cloneArr = (arr) => {
     // let newArr = arr.slice()
     let newArr = [];
-    for (element of arr) {
-        newArr.push(element)
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(arr[i])
     }
 
     return newArr
