f803040d3bf17d5b1c99fbafe68c826c99861a43
rombor <rombor.py@gmail.com>
2020-01-28 10:44:54 +0200

    GromCode JS

--- hw6/index.js
+++ hw6/index.js
@@ -1,9 +1,6 @@
-const getSpecialNumbers = (m, n) => {
-    let specialNumbers = [];
-    for (let i = m; i <= n; i++) {
-        if (i % 3 === 0) {
-            specialNumbers.push(i)
-        }
-    }
-    return specialNumbers
+const swap = (arr) => {
+    const [firstElement, ...args] = arr
+    const newArr = [...args, firstElement]
+
+    return newArr
 }
