22b27fe69fe6558e36b086c21a050b3294cef8d7
rombor <rombor.py@gmail.com>
2020-01-29 23:30:55 +0200

    GromCode JS

--- lesson_9/hw1/index.js
+++ lesson_9/hw1/index.js
@@ -1,4 +1,7 @@
 const getCustomersList = customersObj =>
-    Object.entries(customersObj)
+    // Create a deep copy of getting object ant transform it to array
+    Object.entries(JSON.parse(JSON.stringify(customersObj)))
+    // ['id_1', {name: "William", age: 54}] => [{name: "William", age: 54, id: "customer-id-1"}]
     .map(element => Object.assign(element[1], {'id': element[0]}))
     .sort((a, b) => a.age - b.age)
+    
