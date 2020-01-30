a404f798890f0f61d3086618709fbb3ae5d7d589
rombor <rombor.py@gmail.com>
2020-01-30 11:35:37 +0200

    GromCode JS

--- lesson_10/lessonTasks/index.js
+++ lesson_10/lessonTasks/index.js
@@ -1,8 +1,8 @@
 const getFiniteNumbers = array => array
-    .filter(element => !Number.isFinite(element))
+    .filter(element => Number.isFinite(element))
 
 const getFiniteNumbersV2 = array => array
-    .filter(element => !isFinite(element))
+    .filter(element => isFinite(element))
 
 const getNaN = array => array
     .filter(element => Number.isNaN(element))
