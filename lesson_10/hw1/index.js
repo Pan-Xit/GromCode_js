73fcfc4852cddee6ecde431a93bfbb23d07eeb4b
rombor <rombor.py@gmail.com>
2020-01-30 12:14:16 +0200

    GromCode JS

--- lesson_10/hw1/index.js
+++ lesson_10/hw1/index.js
@@ -1,5 +1,5 @@
 const getTotalPrice = numbersArray => {
     const result = numbersArray.reduce((acc, element) => acc + element)
 
-    return '$' + Math.trunc(result * 100) / 100
+    return '$' + Math.floor(result * 100) / 100
 }
