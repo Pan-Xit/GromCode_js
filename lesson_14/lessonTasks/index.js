f7b06cd20398ab85f00acf31f8eda738f7cf24e8
rombor <rombor.py@gmail.com>
2020-02-02 16:23:21 +0200

    GromCode JS

--- lesson_14/lessonTasks/index.js
+++ lesson_14/lessonTasks/index.js
@@ -1,8 +1,7 @@
 export default function createMessenger() {
     // let message = 'Just learn it!';
-    // let sender = 'GromCode';
+    let sender = 'GromCode';
     let message;
-    let sender;
 
     const sendMessage = (name) => {
         console.log(`${name}, ${message} Your ${sender}`)
