ac8830dc2e1fc058036c41d047deb89ff3074639
rombor <rombor.py@gmail.com>
2020-02-02 15:47:26 +0200

    GromCode JS

--- lesson_14/lessonTasks/index.js
+++ lesson_14/lessonTasks/index.js
@@ -1,9 +1,9 @@
-let message = 'Just learn it!';
+let message = 'Just learn it';
 
 export const sendMessage = (name) => {
     const sender = 'GromCode';
 
-    console.log(`${name}, ${message}! Your ${sender}`)
+    console.log(`${name}, ${message}! Your Gromcode`)
 }
 
 export const setMessage = (text) => {
