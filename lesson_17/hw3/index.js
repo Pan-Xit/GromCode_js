ef09438487b76c0cf863e22725595f5c4dfeb146
rombor <rombor.py@gmail.com>
2020-02-04 20:26:51 +0200

    GromCode JS

--- lesson_17/hw3/index.js
+++ lesson_17/hw3/index.js
@@ -1,5 +1,5 @@
-export const bind = (func, context) => {
-    return function() {
+export const bind = (func, context, ...args) => {
+    return function(...args) {
         return func.call(context, ...arguments)
     }
 }
