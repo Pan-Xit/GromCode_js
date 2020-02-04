c9a128cead73b65b7b55b823529a1b671c502cb9
rombor <rombor.py@gmail.com>
2020-02-04 15:24:18 +0200

    GromCode JS

--- lesson_17/lessonTasks/index.js
+++ lesson_17/lessonTasks/index.js
@@ -1,25 +1,5 @@
-const defer = (func, ms) => {
+export const defer = (func, ms) => {
     return function() {
         setTimeout(() => func.call(this, ...arguments), ms)
     }
 }
-
-// const sayHi = (name) => {
-//     console.log(`Hi, ${name}`)
-// }
-
-// const deferSayHi = defer(sayHi, 1000);
-
-// deferSayHi('Tom')
-
-// const user = {
-//     name: 'Bob',
-
-//     sayHi() {
-//         console.log('Hello,', this.name)
-//     }
-// }
-
-// const deferUserSayHi = defer(user.sayHi, 1000)
-
-// deferUserSayHi.call(user)
