515070a9153279784a200efce8dc41314861ec95
rombor <rombor.py@gmail.com>
2020-02-05 15:34:38 +0200

    GromCode JS

--- lesson_17/hw3/index.js
+++ lesson_17/hw3/index.js
@@ -1,5 +1,6 @@
 export const bind = (func, context, ...args) => {
     return function() {
+        console.log(arguments)
         return func.call(context, ...args, ...arguments)
     }
 }
---
+++ lesson_18/index.js
@@ -0,0 +1,23 @@
+const event = {
+    message: 'Welcome to the party!',
+    guests: [
+        { 
+            name: 'John', 
+            age: 18, 
+            email: 'example@server.com', 
+        },
+        {
+            name: 'SomeAnotherGuest', 
+            age: 16, 
+            email: 'another@server.com', 
+        }
+    ],
+    getInvitation() {
+        return this.guests.filter(({ age }) => age >= 18).map(({ name, email }) => {
+            return {
+                email,
+                message: `Dear ${name}! ${this.message}`
+            }
+        });
+    },
+}
