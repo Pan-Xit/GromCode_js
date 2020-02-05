58958c4700344574dec43ea6ac78ec247141c27d
rombor <rombor.py@gmail.com>
2020-02-05 15:57:14 +0200

    GromCode JS

--- lesson_18/index.js
+++ lesson_18/index.js
@@ -1,9 +1,5 @@
-export const wallet = {
-    transactions: [1, 2, 11, 42],
-    getMax() {
-        return Math.max(...this.transactions)
-    },
-    getMin() {
-        return Math.min(...this.transactions)
-    },
+export function sumOfSquares() {
+    return [...arguments].reduce((acc, element) => {
+        return acc + element * element
+    }, 0)
 }
