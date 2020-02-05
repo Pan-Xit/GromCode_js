40dcc1b472f5df59281ddbe489eed83f152c1620
rombor <rombor.py@gmail.com>
2020-02-05 15:41:25 +0200

    GromCode JS

--- lesson_18/index.js
+++ lesson_18/index.js
@@ -1,18 +1,9 @@
 export const event = {
     message: 'Welcome to the party!',
     guests: [
-        { 
-            name: 'John', 
-            age: 18, 
-            email: 'example@server.com', 
-        },
-        {
-            name: 'SomeAnotherGuest', 
-            age: 16, 
-            email: 'another@server.com', 
-        }
+        { name: 'John', age: 18, email: 'example@server.com', },
     ],
-    getInvitation() {
+    getInvitations() {
         return this.guests.filter(({ age }) => age >= 18).map(({ name, email }) => {
             return {
                 email,
