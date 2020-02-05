ac4c0cb5d0dfc03bb2d9280b6870ed61fe5f6f34
rombor <rombor.py@gmail.com>
2020-02-05 20:43:02 +0200

    GromCode JS

--- lesson_19/lessonsTask/index.js
+++ lesson_19/lessonsTask/index.js
@@ -1,22 +1,4 @@
-export const vehicle = {
-    move() {
-        console.log(`${this.name} is moving`);
-    },
-    stop() {
-        console.log(`${this.name} stopped`)
-    }
-};
-
-export const ship = {
-    name: 'Argo',
-    startMachine() {
-        console.log(`${this.name} lifting anchor up`);
-        this.move();
-    },
-    stopMachine() {
-        this.stop();
-        console.log(`${this.name} lifting anchor down`)
-    },
-
-    __proto__: vehicle,
-}
+export const getOwnProps = (obj) => 
+    Object.entries(obj)
+    .filter(([_, value]) => typeof value !== 'function')
+    .map(([key, _]) => key)
