2ed04296a42f5864107aa34337817ffe057f6e44
rombor <rombor.py@gmail.com>
2020-02-06 17:21:59 +0200

    GromCode JS

---
+++ lesson_20/hw1/index.js
@@ -0,0 +1,37 @@
+export class Order {
+    constructor(price, city, type) {
+        this.id = Math.random().toString();
+        this.price = price;
+        this.dateCreated = new Date();
+        this.isConfirmed = false;
+        this.dateConfirmed = null;
+        this.city = city;
+        this.type = type;
+    }
+
+    checkPrice() {
+        if (this.price > 1000) {
+            return true;
+        }
+
+        return false;
+    }
+
+    confirmOrder() {
+        if (this.isConfirmed === false) {
+            this.isConfirmed = true;
+        }
+
+        this.dateConfirmed = new Date();
+    }
+
+    isValidType() {
+        switch (this.type) {
+            case ('Buy'):
+            case ('Sell'):
+                return true
+            default:
+                return false
+        }
+    }
+}
