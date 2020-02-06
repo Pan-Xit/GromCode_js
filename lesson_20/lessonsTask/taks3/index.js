0e15473d79a76cb05fee5f019fb3aa8386c23e4e
rombor <rombor.py@gmail.com>
2020-02-06 16:59:40 +0200

    GromCode JS

---
+++ lesson_20/lessonsTask/taks3/index.js
@@ -0,0 +1,20 @@
+export class Wallet {
+    balance = 0
+
+    getBalance() {
+        return this.balance
+    }
+
+    deposit(amount) {
+        this.balance += amount;
+    }
+
+    withdraw(amount) {
+        if (amount > this.balance) {
+            console.log('No enough funds')
+            return;
+        }
+
+        this.balance -= amount;
+    }
+}
