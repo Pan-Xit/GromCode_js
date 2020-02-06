b2e9be30186529c9f55845603d913e9309b7bb92
rombor <rombor.py@gmail.com>
2020-02-06 16:14:47 +0200

    GromCode JS

---
+++ lesson_20/lessonsTask/index.js
@@ -0,0 +1,25 @@
+export class User {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    sayHi() {
+        console.log(`Hi, I am ${this.name}`);
+    }
+
+    requestNewPhoto() {
+        console.log(`New photo request was sent for ${this.name}`);
+    }
+
+    setAge(age) {
+        if (age < 0) {
+            return false
+        } else if (age >= 25) {
+            this.requestNewPhoto()
+        }
+        
+        this.age = age;
+
+    }
+}
