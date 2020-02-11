3133fb0b4053589b48c7e3bb83a14285e899f4e6
rombor <rombor.py@gmail.com>
2020-02-11 16:08:37 +0200

    GromCode JS

--- lesson_24/hw1/index.js
+++ lesson_24/hw1/index.js
@@ -1,19 +1,25 @@
 export const getDiff = (startDate, endDate) => {
-    const diff = endDate.getTime() - startDate.getTime()
-    const time = {
-        day: 1000 * 60 * 60 * 24,
-        hour: 1000 * 60 * 60,
-        min: 1000 * 60,
-        sec: 1000,
-        // ms: 1,
+    const dateToUTCTimestamp = (date) => {
+        const offset = date.getTimezoneOffset()
+        return date.getTime() - offset * 6e4
     }
+
+    const diff = dateToUTCTimestamp(endDate) - dateToUTCTimestamp(startDate);
+    const time = {
+        day: 1e3 * 60 * 60 * 24,
+        hour: 1e3 * 60 * 60,
+        min: 1e3 * 60,
+        sec: 1e3,
+        // ms: 1,
+    };
     
     const timeDiff = {};
 
     Object.entries(time).reduce((acc, [time, value]) => {
         Object.assign(timeDiff, {[time]: Math.trunc(acc / value)});
         return acc % value;
-    }, diff)
+    }, diff);
 
     return `${timeDiff.day}d ${timeDiff.hour}h ${timeDiff.min}m ${timeDiff.sec}s`
+
 }
