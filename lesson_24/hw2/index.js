635c07e065d6230024ef8f589b118eb5b691650c
rombor <rombor.py@gmail.com>
2020-02-11 17:46:58 +0200

    GromCode JS

--- lesson_24/hw2/index.js
+++ lesson_24/hw2/index.js
@@ -14,7 +14,7 @@ const month = {
 }
 
 export const studentsBirthDays = (students) => {   
-    const formatedStuds = stud
+    const formatedStuds = students
         .map(({ name, birthDate }) => {
             return { name, birthDate: new Date(Date.parse(birthDate))}
         }).sort((a, b) => a.birthDate.getTime() - b.birthDate.getTime())
@@ -33,3 +33,16 @@ export const studentsBirthDays = (students) => {
 
     return studBirthdays;
 }
+
+
+// const stud = [
+//     { name: '1', birthDate: '01/15/2010' },
+//     { name: '4', birthDate: '05/15/2010' },
+//     { name: '3', birthDate: '02/20/2010' },
+//     { name: '2', birthDate: '02/15/2010' },
+//     { name: '6', birthDate: '08/15/2010' },
+//     { name: '5', birthDate: '08/08/2010' },
+// ]
+
+
+// console.log(studentsBirthDays(studs))
