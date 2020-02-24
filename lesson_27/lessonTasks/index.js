d2791e98fde514585d8f8610cced13344363821d
rombor <rombor.py@gmail.com>
2020-02-24 19:58:40 +0200

    GromCode JS

---
+++ lesson_27/lessonTasks/index.js
@@ -0,0 +1,8 @@
+export function getLocalStorageData() {
+    return Object.entries(localStorage).reduce((acc, [key, value]) => {
+        return {
+            ...acc,
+            [key]: JSON.parse(value)
+        }
+    }, {})
+}
