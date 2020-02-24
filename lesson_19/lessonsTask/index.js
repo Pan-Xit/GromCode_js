9dee7ed5b6ad81b49578b65de9aa9844012a3cdf
rombor <rombor.py@gmail.com>
2020-02-24 20:09:29 +0200

    GromCode JS

--- lesson_19/lessonsTask/index.js
+++ lesson_19/lessonsTask/index.js
@@ -17,31 +17,3 @@ export default {
         this.lastName = lastName;
     }
 }
-
-// export function user(firstName = 'John', lastName = 'Doe') {
-//     this.firstName = firstName;
-//     this.lastName = lastName;
-// }
-
-
-// export class user {
-//     constructor() {
-//         this.firstName = 'John';
-//         this.lastName = 'Doe';
-//     }
-
-//     get fullName() {
-//         return `${this.firstName} ${this.lastName}`
-//     }
-
-//     set fullName(name) {
-//         const [firstName, lastName] = name.split(' ');
-
-//         if (!firstName || !lastName) {
-//             return 'Wrong name format, should be "Name Surname"'
-//         }
-
-//         this.firstName = firstName;
-//         this.lastName = lastName;
-//     }
-// }
