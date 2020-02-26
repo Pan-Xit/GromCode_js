078f032f109ce48051e38847702b64a4443ce3f7
rombor <rombor.py@gmail.com>
2020-02-26 17:17:07 +0200

    GromCode JS

---
+++ lesson_28/lessonTasks/task1/index.js
@@ -0,0 +1,29 @@
+function calc(num) {
+    let result = num
+
+    return {
+        add(num) {
+            result += num;
+            return this
+        },
+
+        subtract(num) {
+            result -= num;
+            return this
+        },
+
+        mult(num) {
+            result *= num;
+            return this
+        },
+
+        div(num) {
+            result /= num;
+            return this
+        },
+
+        result() {
+            return result
+        }
+    }
+}
