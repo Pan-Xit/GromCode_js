7a8e792f8c5ffa2e4cb69e53f357be50b646f062
rombor <rombor.py@gmail.com>
2020-02-27 15:49:04 +0200

    GromCode JS

---
+++ lesson_29/lessonTasks/task1/index.js
@@ -0,0 +1,18 @@
+export function addImage(url, callback) {
+    const pageElem = document.querySelector('.page');
+
+    const imgElem = document.createElement('img');
+    imgElem.setAttribute('alt', 'Some picture');
+    imgElem.src = url
+
+    pageElem.append(imgElem)
+    imgElem.addEventListener('load', () => {
+        const { width, height } = imgElem;
+        callback(null, {width, height})
+    })
+
+    imgElem.addEventListener('error', () => {
+        const errorMessage = 'Image load failed'
+        callback(errorMessage)
+    })
+}
