f31926273d432434b402015cb10aa7d8b1c46d22
rombor <rombor.py@gmail.com>
2020-02-28 20:43:38 +0200

    GromCode JS

--- lesson_31/lessonTasks/task2/index.js
+++ lesson_31/lessonTasks/task2/index.js
@@ -4,19 +4,17 @@ export const asyncCalculator = (number) => new Promise(resolve => {
             resolve(number);
         }, 500)
     })
-    .then(value => {
-        const result = value * value;
+    .then(value => new Promise(resolve => {
         setTimeout(() => {
+            const result = value * value;
             console.log(`Squared value: ${result}`);
+            resolve(result);
         }, 500);
-
-        return result;
-    })
-    .then(value => {
-        const result = value * 2;
+    }))
+    .then(value => new Promise(resolve => {
         setTimeout(() => {
+            const result = value * 2;
             console.log(`Doubled value: ${result}`);
+            resolve(result);
         }, 500);
-
-        return result;
-    })
+    }))
