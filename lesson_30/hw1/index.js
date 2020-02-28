e484babf8b4960df3e723337f9f076060b5d5ba6
rombor <rombor.py@gmail.com>
2020-02-28 14:48:23 +0200

    GromCode JS

--- lesson_30/hw1/index.js
+++ lesson_30/hw1/index.js
@@ -1,38 +1,15 @@
-// import { addImage } from './addImage.js'
+import { addImage } from './addImage.js'
 
 export function addImageV2(url) {
     const p = new Promise((resolve, reject) => {
-        addImage(url, clb);
-
-        clb = (error, data) => {
+        addImage(url, (error, data) => {
             if (error) {
-                reject(new Error(error))
+                reject(error)
+            } else {
+                resolve(data)
             }
-
-            resolve(data)
-        }
+        })
     })
 
     return p
 }
-
-
-const addImage = (url, callback) => {
-    const img = document.createElement('img');
-    img.setAttribute('alt', 'User avatar');
-    img.src = url;
-
-    const pageElem = document.querySelector('.page');
-    pageElem.append(img);
-    
-    const onImageLoaded = () => {
-        const { width, height } = img;
-        callback(null, { width, height });
-    }
-    
-    const onImageLoadError = () => callback('Image load failed');
-    
-    img.addEventListener('load', onImageLoaded);
-    
-    img.addEventListener('error', onImageLoadError);
-};
