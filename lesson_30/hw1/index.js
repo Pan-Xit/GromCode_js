f32db14618305672ab06d4105ad6d70c7f1659a8
rombor <rombor.py@gmail.com>
2020-02-28 15:52:59 +0200

    GromCode JS

--- lesson_30/hw1/index.js
+++ lesson_30/hw1/index.js
@@ -1,31 +1,31 @@
 import { addImage } from './addImage.js'
 
 
-export function addImageV2(url) {
-    const p = new Promise((resolve, reject) => {
-        addImage(url, (error, data) => {
-            if (error) {
-                reject(error)
-            } else {
-                resolve(data)
-            }
-        })
-    })
-
-    return p
-}
-
-
 // export function addImageV2(url) {
 //     const p = new Promise((resolve, reject) => {
-//         const clb = (error, data) => {
+//         addImage(url, (error, data) => {
 //             if (error) {
-//                 reject(new Error(error))
+//                 reject(error)
+//             } else {
+//                 resolve(data)
 //             }
-//             resolve(data)
-//         }
-// 
-//         addImage(url, clb);
+//         })
 //     })
+
 //     return p
 // }
+
+
+export function addImageV2(url) {
+    const p = new Promise((resolve, reject) => {
+        const clb = (error, data) => {
+            if (error) {
+                reject(new Error(error))
+            }
+            resolve(data)
+        }
+
+        addImage(url, clb);
+    })
+    return p
+}
